3cb6eda663b4dc4f39bd947b78cc2b96b1bf5cd3
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-04-01 01:40:19 +0600

    adding currentuser

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@
  const app = express();
  app.use(json());
 
+ app.get('/api/users/currentuser', (req, res) => {
+   res.send('Hi there');
+ });
+
  app.listen(3000, () => {
     console.log('Server running on port 3000!!!!');
  });
